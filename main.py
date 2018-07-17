1200b1f81572b2560e69b6839579a65dffb6b742
Eric Rosko <eric_rosko@hotmail.com>
2018-07-17 07:03:45 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+http://0.0.0.0:6738/98499/
+
+"""
+
+import os
+from flask import Flask
+from time import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_epoch():
+    return str(int(time()))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
